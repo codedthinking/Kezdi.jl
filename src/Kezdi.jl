999c45d0b067c5ef22ff882dcdc85eec3648873f
Miklós Koren <miklos.koren@gmail.com>
2022-12-13 14:46:12 +0100

    Add @regress macros

--- src/Kezdi.jl
+++ src/Kezdi.jl
@@ -1,4 +1,5 @@
 module Kezdi
+export @regress
 
 using Reexport
 @reexport using CSV
@@ -10,5 +11,36 @@ using Reexport
 @reexport using LinearAlgebra
 @reexport using StatFiles
 @reexport using Statistics
+
+# check for macro hygene
+macro regress(df, formula)
+	:(reg($df, @formula($formula)))
+end
+
+macro regress(df, y, xs...)
+	@show terms
+	formula = Expr(:call, :~,
+		y,
+		Expr(:call, :+, xs...)
+	)
+	:(reg($df, @formula($formula)))
+end
+
+@doc"""
+Exports all names defined in the packages.
+
+`@regress` macro defines new syntax for regressions:
+```
+@regress df price ~ 1 + quantity + fe(country)
+```
+and 
+```
+@regress df price quantity fe(country)
+```
+both evaluate to
+```
+reg(df, @formula(price ~ 1 + quantity + fe(country))
+```
+"""
 
 end # module
