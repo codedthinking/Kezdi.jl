3dc9f85db296fbe01808f7583d52b7bcd86c7914
Miklós Koren <miklos.koren@gmail.com>
2024-05-23 21:17:13 +0200

    Test dispatch in codegen.jl

--- src/codegen.jl
+++ src/codegen.jl
@@ -1,6 +1,10 @@
 # use multiple dispatch to generate code 
 generate(command::Command) = generate(Val(command.command), command)
 
-function generate(::Value{:keep}, command::Command)::Vector{Expr}
+function generate(::Val{:keep}, command::Command)::Vector{Expr}
     return [:(select($(command.arguments...)))]
+end
+
+function generate(::Val{:replace}, command::Command)::Vector{Expr}
+    return [:(transform($(command.arguments...)))]
 end
