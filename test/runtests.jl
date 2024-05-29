d580b7f5868e63fac8d7fadc30f2725a204c3d8c
Miklós Koren <koren@localhost>
2024-05-29 15:15:31 +0200

    Fix tests: there can only be one condition for each command
    22/23 tests pass, remaining failure: "@summarize x @if x < 0 && y > 0 "

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,18 +2,18 @@ using Test
 include("../src/transpiler.jl")
 
 TEST_CASES = [
-    (ex="@keep a b", command=:keep, arguments=[:a, :b], condition=[], options=[]),
-    (ex="@generate d = 1", command=:generate, arguments=[:(d = 1)], condition=[], options=[]),
-    (ex="@summarize d", command=:summarize, arguments=[:d], condition=[], options=[]),
-    (ex="@regress y x, robust", command=:regress, arguments=[:y, :x], condition=[], options=[:robust]),
-    (ex="@regress y x, absorb(country)", command=:regress, arguments=[:y, :x], condition=[], options=[:(absorb(country))]),
-    (ex="@regress y log(x), robust", command=:regress, arguments=[:y, :(log(x))], condition=[], options=[:robust]),
-    (ex="@summarize x, detail", command=:summarize, arguments=[:x], condition=[], options=[:detail]),
-    (ex="@summarize x @if x < 0", command=:summarize, arguments=[:x], condition=[:(x < 0)], options=[]),
-    (ex="@summarize x @if ln(x) < 0", command=:summarize, arguments=[:x], condition=[:(ln(x) < 0)], options=[]),
-    (ex="@summarize x @if x < 0, detail", command=:summarize, arguments=[:x], condition=[:(x < 0)], options=[:detail]),
-    (ex="@summarize x @if x < 0 && y > 0", command=:summarize, arguments=[:x], condition=[:(x < 0 && y > 0)], options=[]),   
-    (ex="@summarize x @if x < 0 && y > 0, detail", command=:summarize, arguments=[:x], condition=[:(x < 0 && y > 0)], options=[:detail]),   
+    (ex="@keep a b", command=:keep, arguments=[:a, :b], condition=nothing, options=[]),
+    (ex="@generate d = 1", command=:generate, arguments=[:(d = 1)], condition=nothing, options=[]),
+    (ex="@summarize d", command=:summarize, arguments=[:d], condition=nothing, options=[]),
+    (ex="@regress y x, robust", command=:regress, arguments=[:y, :x], condition=nothing, options=[:robust]),
+    (ex="@regress y x, absorb(country)", command=:regress, arguments=[:y, :x], condition=nothing, options=[:(absorb(country))]),
+    (ex="@regress y log(x), robust", command=:regress, arguments=[:y, :(log(x))], condition=nothing, options=[:robust]),
+    (ex="@summarize x, detail", command=:summarize, arguments=[:x], condition=nothing, options=[:detail]),
+    (ex="@summarize x @if x < 0", command=:summarize, arguments=[:x], condition=:(x < 0), options=[]),
+    (ex="@summarize x @if ln(x) < 0", command=:summarize, arguments=[:x], condition=:(ln(x) < 0), options=[]),
+    (ex="@summarize x @if x < 0, detail", command=:summarize, arguments=[:x], condition=:(x < 0), options=[:detail]),
+    (ex="@summarize x @if x < 0 && y > 0", command=:summarize, arguments=[:x], condition=:(x < 0 && y > 0), options=[]),   
+    (ex="@summarize x @if x < 0 && y > 0, detail", command=:summarize, arguments=[:x], condition=:(x < 0 && y > 0), options=[:detail]),   
 ]
 
 macro return_arguments(expr)
@@ -40,10 +40,10 @@ end
 
 @testset "Condition is parsed" begin
     @testset "$(case.ex)" for case in TEST_CASES
-        if length(case.condition) > 0
+        if !isnothing(case.condition)
             expressions = preprocess(case.ex)
             command = transpile(expressions, case.command)
-            @warn typeof(command.condition)
+            @warn typeof(command.condition), typeof(case.condition)
             @test command.condition == case.condition
         end
     end
