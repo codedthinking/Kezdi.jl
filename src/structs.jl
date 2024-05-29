fad6051c3fc5f1476d0da3b48fedc65d770b611c
Miklós Koren <koren@localhost>
2024-05-29 16:19:18 +0200

    Add df

--- src/structs.jl
+++ src/structs.jl
@@ -7,7 +7,11 @@ end
 
 struct Command 
     command::Symbol
+    df::Any
     arguments::Tuple
     condition::Union{Expr, Nothing}
     options::Tuple
 end
+
+# if DataFrame is not explicitly defined, use the first argument
+Command(command, arguments, condition, options) = Command(command, arguments[1], arguments[2:end], condition, options)
