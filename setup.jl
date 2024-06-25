c47e0bb8b4863cb904bee0281d66a59bdccbfa28
Gergely Attila Kiss <corra971407@gmail.com>
2024-06-25 15:55:50 +0200

    trying setup.jl

---
+++ setup.jl
@@ -0,0 +1,7 @@
+using Pkg
+
+# Add the dependency from URL
+Pkg.add(url="https://github.com/codedthinking/With.jl.git")
+
+# Add your package
+Pkg.add(url="https://github.com/codedthinking/Kezdi.jl.git")
