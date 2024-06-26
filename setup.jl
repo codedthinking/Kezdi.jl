fd00efa673231b042681f94e3b7a95759a6f4d95
Gergely Attila Kiss <corra971407@gmail.com>
2024-06-26 11:56:06 +0200

    Revert "trying setup.jl"
    This reverts commit c47e0bb8b4863cb904bee0281d66a59bdccbfa28.

--- setup.jl
+++
@@ -1,7 +0,0 @@
-using Pkg
-
-# Add the dependency from URL
-Pkg.add(url="https://github.com/codedthinking/With.jl.git")
-
-# Add your package
-Pkg.add(url="https://github.com/codedthinking/Kezdi.jl.git")
