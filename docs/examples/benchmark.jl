6bc546518dc19397958bd4c9b38af04a23586ec9
Miklós Koren <miklos.koren@gmail.com>
2024-07-11 15:33:56 +0200

    Update speed benchmarks

---
+++ docs/examples/benchmark.jl
@@ -0,0 +1,22 @@
+using Kezdi
+using Random
+using BenchmarkTools
+using Pkg; Pkg.precompile()
+
+df = DataFrame(i = 1:10_000_000)
+df.g = rand(0:99, nrow(df))
+
+println("Egen")
+@btime @with df  @egen mean_i = mean(i), by(g)
+
+println("Collapse")
+@btime @with df  @collapse mean_i = mean(i), by(g)
+
+println("Tabulate")
+@btime @with df  @tabulate g
+
+println("Summarize")
+@btime @with df  @summarize g
+
+println("Regress")
+@btime @with df  @regress i g @if g > 50
