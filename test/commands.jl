5462909acf3ee7b23dc67c5889049f10f663e8d8
Miklós Koren <miklos.koren@gmail.com>
2024-07-12 23:54:48 +0200

    Use `promote_type` for type promotion when replacing a variable
    Fixed #148
    Fixed #156
    
    325/325 tests pass

--- test/commands.jl
+++ test/commands.jl
@@ -92,11 +92,11 @@ end
         @test eltype((@with df @replace x = missing @if _n == 1).x) == Union{Missing, Int}
         @test eltype((@with df @replace x = "a" @if _n == 1).x) == Any
         df = DataFrame(x=[missing, 2, 3])
-        @test eltype((@with df @replace x = 1 @if _n == 1).x) == Int
+        @test eltype((@with df @replace x = 1 @if _n == 1).x) == Union{Int, Missing}
         df = DataFrame(x=[1.1, 2, 3])
-        @test eltype((@with df @replace x = 1 @if _n == 1).x) == Int
+        @test eltype((@with df @replace x = 1 @if _n == 1).x) <: AbstractFloat
         df = DataFrame(x=[1, 2, missing])
-        @test eltype((@with df @replace x = 1.1 @if _n == 1).x) == Union{T, Missing} where T <: AbstractFloat
+        @test eltype((@with df @replace x = 1.1 @if _n == 1).x) <: Union{T, Missing} where T <: AbstractFloat
     end
 
     @testset "Error handling" begin
