8b8e6efda1110110f39833d56f2d210ba977bbd3
Miklós Koren <miklos.koren@gmail.com>
2024-07-14 10:12:31 +0200

    Revert back to 45c5ddb8d35449ac406ccbb838102c8f234c962f version of With.jl

--- src/With.jl
+++ src/With.jl
@@ -1,30 +1,246 @@
 module With
-using ..Kezdi
 export @with, @with!
+using ..Kezdi
 
-"""
-    @with df begin
-        # do something with df
+is_aside(x) = false
+function is_aside(x::Expr)::Bool
+    if x.head == :(=)
+        return is_aside(x.args[2])
+    end
+    return x.head == :macrocall && x.args[1] in Kezdi.SIDE_EFFECTS 
+end
+
+
+insert_first_arg(symbol::Symbol, firstarg; assignment = false) = Expr(:call, symbol, firstarg)
+insert_first_arg(any, firstarg; assignment = false) = insertionerror(any)
+
+function insertionerror(expr)
+    error(
+        """Can't insert a first argument into:
+        $expr.
+
+        First argument insertion works with expressions like these, where [Module.SubModule.] is optional:
+
+        [Module.SubModule.]func
+        [Module.SubModule.]func(args...)
+        [Module.SubModule.]func(args...; kwargs...)
+        [Module.SubModule.]@macro
+        [Module.SubModule.]@macro(args...)
+        @. [Module.SubModule.]func
+        """
+    )
+end
+
+is_moduled_symbol(x) = false
+function is_moduled_symbol(e::Expr)
+    e.head == :. &&
+        length(e.args) == 2 &&
+        (e.args[1] isa Symbol || is_moduled_symbol(e.args[1])) &&
+        e.args[2] isa QuoteNode &&
+        e.args[2].value isa Symbol
+end
+
+function insert_first_arg(e::Expr, firstarg; assignment = false)
+    head = e.head
+    args = e.args
+    # variable = ...
+    # set assignment = true and rerun with right hand side
+    if !assignment && head == :(=) && length(args) == 2
+        if !(args[1] isa Symbol)
+            error("You can only use assignment syntax with a Symbol as a variable name, not $(args[1]).")
+        end
+        variable = args[1]
+        righthandside = insert_first_arg(args[2], firstarg; assignment = true)
+        :($variable = $righthandside)
+    # Module.SubModule.symbol
+    elseif is_moduled_symbol(e)
+        Expr(:call, e, firstarg)
+
+    # f(args...) --> f(firstarg, args...)
+    elseif head == :call && length(args) > 0
+        if length(args) ≥ 2 && Meta.isexpr(args[2], :parameters)
+            Expr(head, args[1:2]..., firstarg, args[3:end]...)
+        elseif args[1] in [:env, :scalars]
+            # does not have to insert first argument into $e
+            Expr(head, args...)
+        else
+            Expr(head, args[1], firstarg, args[2:end]...)
+        end
+
+    # f.(args...) --> f.(firstarg, args...)
+    elseif head == :. &&
+            length(args) > 1 &&
+            args[1] isa Symbol &&
+            args[2] isa Expr &&
+            args[2].head == :tuple
+
+        Expr(head, args[1], Expr(args[2].head, firstarg, args[2].args...))
+
+    # @. [Module.SubModule.]somesymbol --> somesymbol.(firstarg)
+    elseif head == :macrocall &&
+            length(args) == 3 &&
+            args[1] == Symbol("@__dot__") &&
+            args[2] isa LineNumberNode &&
+            (is_moduled_symbol(args[3]) || args[3] isa Symbol)
+
+        Expr(:., args[3], Expr(:tuple, firstarg))
+
+    # @macro(args...) --> @macro(firstarg, args...)
+    elseif head == :macrocall &&
+        (is_moduled_symbol(args[1]) || args[1] isa Symbol) &&
+        args[2] isa LineNumberNode
+        if args[1] == Symbol("@__dot__")
+            error("You can only use the @. macro and automatic first argument insertion if what follows is of the form `[Module.SubModule.]func`")
+        end
+
+        if length(args) >= 3 && args[3] isa Expr && args[3].head == :parameters
+            # macros can have keyword arguments after ; as well
+            Expr(head, args[1], args[2], args[3], firstarg, args[4:end]...)
+        else
+            Expr(head, args[1], args[2], firstarg, args[3:end]...)
+        end
+
+    else
+        insertionerror(e)
+    end
+end
+
+function rewrite(expr, replacement)
+    aside = is_aside(expr)
+    new_expr = insert_first_arg(expr, replacement)
+    if !aside
+        replacement = gensym()
+        new_expr = :(local $replacement = $new_expr)
+    else
+        new_expr = :(display($new_expr))
     end
 
-The `@with` macro is a convenience macro that allows you to set the current data frame and perform operations on it in a single block. The first argument is the data frame to set as the current data frame, and the second argument is a block of code to execute. The data frame is set as the current data frame for the duration of the block, and then restored to its previous value after the block is executed.
+    (new_expr, replacement)
+end
 
-The macro returns the value of the last expression in the block.
+rewrite(l::LineNumberNode, replacement) = (l, replacement)
+
+function rewrite_with_block(firstpart, block)
+    pushfirst!(block.args, firstpart)
+    rewrite_with_block(block)
+end
+
+"""
+    @with(expr, exprs...)
+
+Rewrites a series of expressions into a with, where the result of one expression
+is inserted into the next expression following certain rules.
+
+**Rule 1**
+
+Any `expr` that is a `begin ... end` block is flattened.
+For example, these two pseudocodes are equivalent:
+
+```julia
+@with a b c d e f
+
+@with a begin
+    b
+    c
+    d
+end e f
+```
+
+**Rule 2**
+
+Any expression but the first (in the flattened representation) will have the preceding result
+inserted as its first argument, unless at least one underscore `_` is present.
+In that case, all underscores will be replaced with the preceding result.
+
+If the expression is a symbol, the symbol is treated equivalently to a function call.
+
+For example, the following code block
+
+```julia
+@with begin
+    x
+    f()
+    @g()
+    h
+    @i
+    j(123, _)
+    k(_, 123, _)
+end
+```
+
+is equivalent to
+
+```julia
+begin
+    local temp1 = f(x)
+    local temp2 = @g(temp1)
+    local temp3 = h(temp2)
+    local temp4 = @i(temp3)
+    local temp5 = j(123, temp4)
+    local temp6 = k(temp5, 123, temp5)
+end
+```
+
+**Rule 3**
+
+An expression that begins with `@aside` does not pass its result on to the following expression.
+Instead, the result of the previous expression will be passed on.
+This is meant for inspecting the state of the with.
+The expression within `@aside` will not get the previous result auto-inserted, you can use
+underscores to reference it.
+
+```julia
+@with begin
+    [1, 2, 3]
+    filter(isodd, _)
+    @aside @info "There are \$(length(_)) elements after filtering"
+    sum
+end
+```
+
+**Rule 4**
+
+It is allowed to start an expression with a variable assignment.
+In this case, the usual insertion rules apply to the right-hand side of that assignment.
+This can be used to store intermediate results.
+
+```julia
+@with begin
+    [1, 2, 3]
+    filtered = filter(isodd, _)
+    sum
+end
+
+filtered == [1, 3]
+```
+
+**Rule 5**
+
+The `@.` macro may be used with a symbol to broadcast that function over the preceding result.
+
+```julia
+@with begin
+    [1, 2, 3]
+    @. sqrt
+end
+```
+
+is equivalent to
+
+```julia
+@with begin
+    [1, 2, 3]
+    sqrt.(_)
+end
+```
+
 """
 macro with(initial_value, args...)
     block = flatten_to_single_block(initial_value, args...)
     rewrite_with_block(block)
 end
 
-"""
-    @with! df begin
-        # do something with df
-    end
 
-The `@with!` macro is a convenience macro that allows you to set the current data frame and perform operations on it in a single block. The first argument is the data frame to set as the current data frame, and the second argument is a block of code to execute. The data frame is set as the current data frame for the duration of the block, and then restored to its previous value after the block is executed.
-
-The macro does not have a return value, it overwrites the data frame directly.
-"""
 macro with!(initial_value, args...)
     block = flatten_to_single_block(initial_value, args...)
     result = rewrite_with_block(block)
@@ -51,25 +267,29 @@ function rewrite_with_block(block)
 
     reconvert_docstrings!(block_expressions)
 
+    # assign first line to first gensym variable
+    firstvar = gensym()
     rewritten_exprs = []
+    replacement = firstvar
 
     did_first = false
-    local df
     for expr in block_expressions
         # could be an expression first or a LineNumberNode, so a bit convoluted
         # we just do the firstvar transformation for the first non LineNumberNode
         # we encounter
         if !(did_first || expr isa LineNumberNode)
+            expr = :(local $firstvar = $expr)
             did_first = true
-            df = expr
+            push!(rewritten_exprs, expr)
             continue
         end
         
-        push!(rewritten_exprs, expr)
+        rewritten, replacement = rewrite(expr, replacement)
+        push!(rewritten_exprs, rewritten)
     end
-    result = Expr(:block, rewritten_exprs...)
+    result = Expr(:block, rewritten_exprs..., replacement)
 
-    :($(esc(:(Kezdi.ScopedValues.@with Kezdi.context => setdf($df) $result)) ))
+    :($(esc(result)))
 end
 
 # if a line in a with is a string, it can be parsed as a docstring
