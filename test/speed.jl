a105985bdf20048addec101cd4e9b0464768579c
Miklós Koren <miklos.koren@gmail.com>
2024-09-19 12:54:18 +0200

    Test that generate completes in less than 10 seconds
    - This is for 20m rows and 20 columns, 9 executions of generate log(x)
    - Current time is median time is 4-5 seconds, raising bar to 10 seconds to have some slack

--- test/speed.jl
+++ test/speed.jl
@@ -1,19 +1,20 @@
-using Kezdi
-
-begin
-    N = 20_000_000
-    K = 20
-    df = DataFrame(rand(N, K), :auto)
-    @time @with df begin
-        @generate ln_x1 = log(x1)
-        @generate ln_x2 = log(x2)
-        @generate ln_x3 = log(x3)
-        @generate ln_x4 = log(x4)
-        @generate ln_x5 = log(x5)
-        @generate ln_x6 = log(x6)
-        @generate ln_x7 = log(x7)
-        @generate ln_x8 = log(x8)
-        @generate ln_x9 = log(x9)
+@testset "Generate completes within 10 seconds" begin
+    df = DataFrame(rand(20_000_000, 20), :auto)
+    
+    t = @benchmark let df = $df
+        @with df begin
+            @generate ln_x1 = log(x1)
+            @generate ln_x2 = log(x2)
+            @generate ln_x3 = log(x3)
+            @generate ln_x4 = log(x4)
+            @generate ln_x5 = log(x5)
+            @generate ln_x6 = log(x6)
+            @generate ln_x7 = log(x7)
+            @generate ln_x8 = log(x8)
+            @generate ln_x9 = log(x9)
+        end
     end
-    # 4.397261 seconds (153.62 k allocations: 4.332 GiB, 32.32% gc time, 1.78% compilation time)
+    
+    time = median(t).time / 1e9
+    @test time < 10.0
 end
